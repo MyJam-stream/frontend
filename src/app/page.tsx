e0dff7ede3aefff7760e822385e36a30bbf45365
Ricy <1932353948@qq.com>
2023-11-26 22:07:00 +0800

    feat: delete streams recommendation on landing. page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
 'use client'
+import Link from 'next/link'
 import Button from '@/components/Button'
-import Streams from '@/modules/streams'
+// import Streams from '@/modules/streams'
 
 export default function Home() {
-  const scrollDown = () => {
-    if (typeof window === undefined) return
-    window.scrollTo({ top: window.innerHeight, behavior: 'smooth' })
-  }
-
   return (
     <div className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="flex flex-col justify-center items-center w-[100vw] v-[100vh] mt-[-5rem]">
@@ -31,11 +27,13 @@ export default function Home() {
                 <h2 className="text-white text-[1.6rem] font-bold leading-[53.94px]">
                   STREAM FROM ANYWHERE
                 </h2>
-                <Button fullWidth color="amber" onClick={scrollDown}>
-                  <p className="text-black text-[2rem] font-semibold leading-7">
-                    Launch App
-                  </p>
-                </Button>
+                <Link href="/stream">
+                  <Button fullWidth color="amber">
+                    <p className="text-black text-[2rem] font-semibold leading-7">
+                      Launch App
+                    </p>
+                  </Button>
+                </Link>
               </div>
               <div className="mb-[-5rem]">
                 <img
@@ -50,7 +48,7 @@ export default function Home() {
           </div>
           <div className="stick shrink-0 w-full h-[5rem] bg-gradient-to-b from-green-950 via-gray-800 to-teal-950 rounded-[20px] shadow border-2 border-zinc-600"></div>
         </section>
-        <Streams />
+        {/* <Streams /> */}
       </div>
     </div>
   )
